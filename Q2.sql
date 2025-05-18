e7fd59ad55f1e30146289527e53e9e6f8aedbbeb
hamza-bayyat <130403045+hamza-bayyat@users.noreply.github.com>
2025-05-18 09:19:49 +0300

    Create Q2.sql

---
+++ Q2.sql
@@ -0,0 +1,10 @@
+SELECT
+  refresh_date AS Day,
+  term AS Top_Term,
+  rank
+FROM `bigquery-public-data.google_trends.top_terms`
+WHERE
+  rank = 3
+  AND refresh_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
+GROUP BY Day, Top_Term, rank
+ORDER BY Day DESC
